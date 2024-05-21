c8d4a9d934b138aa97286081d5c8973c7caeb0d9
Josepe Navarro Vicente <jose.navarro.vicente@accenture.com>
2024-05-21 16:39:23 +0200

    Actualizamos el controlador global de las excepciones

--- src/main/java/com/josepdevs/Domain/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/josepdevs/Domain/Exceptions/GlobalExceptionHandler.java
@@ -3,58 +3,47 @@ package com.josepdevs.Domain.Exceptions;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.management.relation.RoleNotFoundException;
-
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import lombok.extern.log4j.Log4j2;
-
 @ControllerAdvice //This annotation applies the exception handler across the whole application = global exception handler. This class can contain multiple methods, each annotated with @ExceptionHandler, to handle different types of exceptions.
-@Log4j2
 public class GlobalExceptionHandler {
 	
-	//CENTRALIZED EXCEPTION MANAGER, built-in and personalized exceptions
+	//MY CUSTOMIZED EXCEPTIONS
 	
+    @ExceptionHandler(MyRuntimeException.class)
+    public ResponseEntity handleMyExceptions(MyRuntimeException ex) {
+        Map<String, String> errorDetails = new HashMap<>();
+        errorDetails.put("The attribute with value: '"+ex.getIllegalAttributeValue()+"' was not valid", ex.getMyErrorMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
+    }
+    
+    
+    //////////BUILT-IN EXCEPTIONS HANDLING TO NOW SHOW STACKTRACE
+    
+    
 	@ExceptionHandler(HttpMessageNotReadableException.class)
-	public ResponseEntity<Map<String, String>> myOwnHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-        //create the map that will contain the errorName and the message 
+	public ResponseEntity myOwnHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
 		Map<String, String> errorDetails = new HashMap<>();
-		//put in the map this generic message, for each exception we will personalize it
-		errorDetails.put("Http Error", "Serialization of the received JSON body failed, maybe some validation of the valueObjects failed.");
-        //return in the body the error
-        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+        errorDetails.put("Http Error", "A problem with the HTTP message occurred and cannot be read properly, maybe it could not be parsed correctly to/from JSON.");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
     }
-
-    @ExceptionHandler(RoleNotFoundException.class)
-    public ResponseEntity<RoleNotFoundException> handleRoleNotFoundException(RoleNotFoundException ex) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON); 
-        return new ResponseEntity<>(ex, headers, HttpStatus.BAD_REQUEST);
-    }
-	
-    @ExceptionHandler(PasswordNotValidException.class)
-    public ResponseEntity<PasswordNotValidException> handlePasswordNotValidError(PasswordNotValidException ex) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON); 
-        return new ResponseEntity<>(ex, headers, HttpStatus.BAD_REQUEST);
-    }
-
+    
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseEntity<IllegalArgumentException> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ResponseEntity<>(ex, HttpStatus.BAD_REQUEST);
+    public ResponseEntity handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorDetails = new HashMap<>();
+        errorDetails.put("Illegal argument error", "The argument/parameter that was in use/used was not expected (illegal argument).");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
     }
 
     @ExceptionHandler(value = NullPointerException.class)
-    public ResponseEntity<Map<String, String>> handleNullPointerException(NullPointerException ex) {
+    public ResponseEntity handleNullPointerException(NullPointerException ex) {
         Map<String, String> errorDetails = new HashMap<>();
-        errorDetails.put("error", "A null pointer exception occurred.");
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+        errorDetails.put("Null resource error", "You tried to use a null resource");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetails);
     }
     
     
