33c25fc59b0da2a6d65b00ed982ae1d6d7c4ae7c
Josepe Navarro Vicente <jose.navarro.vicente@accenture.com>
2024-05-21 12:28:57 +0200

    añadir linea, mejora visual

--- src/main/java/com/josepdevs/Application/GetUser.java
+++ src/main/java/com/josepdevs/Application/GetUser.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.josepdevs.Domain.Exceptions.UserNotFoundException;
-import com.josepdevs.Domain.dto.UserEntity;
+import com.josepdevs.Domain.dto.Users;
 import com.josepdevs.Domain.repository.UserRepository;
 import com.josepdevs.Domain.service.UserFinderService;
 
@@ -17,16 +17,17 @@ public class GetUser {
 
 	@Autowired
     private UserRepository userRepository;
+	
 	@Autowired
     private UserFinderService userFinder;
 	
-	public UserEntity getUser(UUID idUser){
-		Optional<UserEntity> existentUser = userFinder.findById(idUser);
-		UserEntity user = existentUser.orElseThrow( () -> new UserNotFoundException(idUser,"The user with the searched id was not found")); 
+	public Users getUser(UUID idUser){
+		Optional<Users> existentUser = userFinder.findById(idUser);
+		Users user = existentUser.orElseThrow( () -> new UserNotFoundException(idUser,"The user with the searched id was not found")); 
         return user;
 	}	
 	
-	public List<UserEntity> getAll(){
+	public List<Users> getAll(){
 		//el caso de uso llama al repositorio
         return  userRepository.readAll();
 	}	
