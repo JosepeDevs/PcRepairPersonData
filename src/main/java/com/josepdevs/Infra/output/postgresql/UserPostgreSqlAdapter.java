bf5cd893428e075126d76526aeac8b4f4a829eec
Josepe Navarro Vicente <jose.navarro.vicente@accenture.com>
2024-05-21 12:37:00 +0200

    borramos imports inutiles, extraemos servicio y return boolean queries

--- src/main/java/com/josepdevs/Infra/output/postgresql/UserPostgreSqlAdapter.java
+++ src/main/java/com/josepdevs/Infra/output/postgresql/UserPostgreSqlAdapter.java
@@ -3,19 +3,10 @@ package com.josepdevs.Infra.output.postgresql;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.function.Function;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
-import org.springframework.data.repository.query.Param;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.josepdevs.Domain.dto.UserEntity;
+import com.josepdevs.Domain.dto.Users;
 import com.josepdevs.Domain.repository.UserRepository;
 import com.josepdevs.Infra.output.UserJpaRepository;
 
@@ -31,40 +22,37 @@ public class UserPostgreSqlAdapter implements UserRepository{
 	//////////////Commands////////////////
 
 	@Override
-    public void createUser(UserEntity user) {
-		UserEntity userEntity = new UserEntity(
-	            	user.getIdUser(),
-	            	user.getEmail(),
-	                user.getPsswrd(),
-	                user.getName(),
-	                user.getRole(),
-	                user.isActive()
-	        );
-	        userJpaRepository.save(userEntity);
+    public Users createUser(Users user) {
+	        userJpaRepository.save(user);
+	        //after save it should contain the UUID, in the future this will return bool, and the event will publish the UUID
+	        return user;
 	}
 	
 	@Override
-	public void updateUser(UserEntity user) {
-        userJpaRepository.save(user);
+	public boolean updateUser(Users user) {
+		Users updatedUser = userJpaRepository.save(user);
+	    return updatedUser.getIdUser()  !=   null    &&    updatedUser.getIdUser().equals(user.getIdUser());
+
 	}
 	
 	@Override
-	public void deleteUser(UUID idClient){
-		Optional<UserEntity> tempUser = userJpaRepository.findById(idClient);
-		UserEntity user = tempUser.orElse(null); // if there is a user, that's the value, otherwise it will be null
+	public boolean deleteUser(UUID idClient){
+		Optional<Users> tempUser = userJpaRepository.findById(idClient);
+		Users user = tempUser.orElse(null); // if there is a user, that's the value, otherwise it will be null
 		user.setActive(false);
-        userJpaRepository.save(user);
+		Users deletedUser = userJpaRepository.save(user);
+	    return deletedUser.getIdUser()  !=   null    &&    deletedUser.getIdUser().equals(user.getIdUser());
 	}
     
     
 	///////////////queries/////////////
 	@Override
-	public Optional<UserEntity> searchUser(UUID idClient){
+	public Optional<Users> searchUser(UUID idClient){
 		return userJpaRepository.findById(idClient);
 	}
 	
 	@Override
-	public List<UserEntity> readAll(){
+	public List<Users> readAll(){
 		return userJpaRepository.findAll();
     }
 		
