e6386799d8cca84fd75031576142f651e5faa149
Josepe Navarro Vicente <jose.navarro.vicente@accenture.com>
2024-05-21 12:27:30 +0200

    extraido y llamado aqui el servicio que monta el User con UUID y un userDTO

--- src/main/java/com/josepdevs/Application/EditUser.java
+++ src/main/java/com/josepdevs/Application/EditUser.java
@@ -1,13 +1,15 @@
 package com.josepdevs.Application;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.josepdevs.Domain.dto.UserEntity;
+import com.josepdevs.Domain.dto.UserDto;
+import com.josepdevs.Domain.dto.Users;
 import com.josepdevs.Domain.repository.UserRepository;
+import com.josepdevs.Domain.service.UserEntityComposerService;
+
 
 @Component
 public class EditUser {
@@ -15,8 +17,13 @@ public class EditUser {
 	@Autowired
     private UserRepository userRepository;
 	
-	public void updateUser(UUID userId, UserEntity user) {
+	@Autowired
+    private UserEntityComposerService compose;
+	
+	public void updateUser(UUID userId, UserDto userDto) {
 		//el caso de uso llama al repositorio
+		Users user = compose.composeUserEntityWithoutId(userDto);
+		user.setIdUser(userId);
        userRepository.updateUser(user);
 	}
 
