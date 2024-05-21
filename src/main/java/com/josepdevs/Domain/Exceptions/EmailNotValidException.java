a3645dc5e32a86ce0ff92365139906e256d09031
Josepe Navarro Vicente <jose.navarro.vicente@accenture.com>
2024-05-21 13:56:29 +0200

    Cambiado orden mensaje/objeto para mayor cohesion con el proyecto

--- src/main/java/com/josepdevs/Domain/Exceptions/EmailNotValidException.java
+++ src/main/java/com/josepdevs/Domain/Exceptions/EmailNotValidException.java
@@ -11,7 +11,7 @@ public class EmailNotValidException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	public EmailNotValidException(String email, String message) {
+	public EmailNotValidException(String message, String email) {
 		super(message);
 		log.error(message,email);
 	}
