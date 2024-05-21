6e2429e685ae914187e0dc374e39c3204e5c0dbe
Josepe Navarro Vicente <jose.navarro.vicente@accenture.com>
2024-05-21 13:55:43 +0200

    Cambiado orden mensaje/objeto para mayor cohesion con el proyecto

--- src/main/java/com/josepdevs/Domain/dto/valueobjects/Email.java
+++ src/main/java/com/josepdevs/Domain/dto/valueobjects/Email.java
@@ -13,11 +13,11 @@ public class Email {
 		
 		if ( ! email.matches("^[\\w-_\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
 		    // algunos ejemplos aceptados hoola-adios@mundo.com , hola_adioos@mundo.com
-			throw new EmailNotValidException("The email format was not adequate, before the @ we allow the use of -_. and - after the @",email);
+			throw new EmailNotValidException("The email format was not adequate, before the @ we allow the use of -_. and - after the @", email);
 		}
 		
 		if( email.length()>=255 ) {
-			throw new LongInputException("The email length was too much, if saved, it has been truncated.",email);
+			throw new LongInputException("The email length was too much, if saved, it has been truncated.", email);
 		}
 		
 		this.email = email;
