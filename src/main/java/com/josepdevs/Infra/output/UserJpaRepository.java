8224c4a00676cdb061767bafa904eeac9db065d2
Josepe Navarro Vicente <jose.navarro.vicente@accenture.com>
2024-05-21 12:35:29 +0200

    cambiamos JpaRepo a Users en lugar de UsersEntity

--- src/main/java/com/josepdevs/Infra/output/UserJpaRepository.java
+++ src/main/java/com/josepdevs/Infra/output/UserJpaRepository.java
@@ -1,11 +1,12 @@
 package com.josepdevs.Infra.output;
 
+
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.josepdevs.Domain.dto.UserEntity;
+import com.josepdevs.Domain.dto.Users;
 
-public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
+public interface UserJpaRepository extends JpaRepository<Users, UUID> {
 //sin implementación, solo para poder llamar a los métodos de JPARepo
 }
