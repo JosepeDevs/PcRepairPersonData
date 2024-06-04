ad0a32ce437d030786b2c6547002e821075f0084
Jose Pedro <jose.navarro.vicente@accenture.com>
2024-06-04 12:27:55 +0200

    added print port to check loadbalancing

--- src/main/java/com/josepdevs/Infra/rest/GetPersonRestController.java
+++ src/main/java/com/josepdevs/Infra/rest/GetPersonRestController.java
@@ -4,11 +4,11 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.josepdevs.Application.GetPersonData;
@@ -22,6 +22,8 @@ public class GetPersonRestController {
 	private final GetPersonData getPerson;
 	
 	@Autowired PersonDataDto personDto;
+	
+	@Autowired Environment enviroment;
     
 	//inyectamos el caso de uso en el constructor 
 	public GetPersonRestController(GetPersonData getPerson) {
@@ -30,6 +32,7 @@ public class GetPersonRestController {
 	
     @GetMapping("persons")
     public ResponseEntity<List<PersonData>> getAll() {
+    	System.out.println(enviroment.getProperty("local.server.port")); 
     	return ResponseEntity.status(HttpStatus.OK).body(getPerson.getAll());
     }
     
