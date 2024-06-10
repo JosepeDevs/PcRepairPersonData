4d7be9d195245dc02cc7b59a752631e3744e7b5b
Jose Pedro <jose.navarro.vicente@accenture.com>
2024-06-10 14:52:41 +0200

    remane of springboot app

--- src/main/java/com/josepdevs/UserDataManager.java
+++ src/main/java/com/josepdevs/UserDataManager.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PersonManagerApp {
+public class UserDataManager {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonManagerApp.class, args);
+		SpringApplication.run(UserDataManager.class, args);
 	}
 
 }
--- src/test/java/com/josepdevs/UserDataManagerTests.java
+++ src/test/java/com/josepdevs/UserDataManagerTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PersonManagerAppTests {
+class UserDataManagerTests {
 
 	@Test
 	void contextLoads() {
